abf706cf8419a49f272081458733103320acf26d
lijunlong <lijunlong@openresty.com>
2024-09-18 12:20:30 +0000

    check in build-openssl111.sh

---
+++ build-openssl111.sh
@@ -0,0 +1,14 @@
+OPENSSL_VER=1.1.1w
+OPENSSL_PATCH_VER=1.1.1f
+OPENSSL_PREFIX=/opt/ssl
+wget https://github.com/openssl/openssl/releases/download/OpenSSL_1_1_1w/openssl-$OPENSSL_VER.tar.gz
+rm -fr openssl-$OPENSSL_VER
+tar -xf openssl-$OPENSSL_VER.tar.gz
+cd openssl-$OPENSSL_VER
+
+patch -p1 < ../openresty/patches/openssl-$OPENSSL_PATCH_VER-sess_set_get_cb_yield.patch;
+./config shared enable-ssl3 enable-ssl3-method -g --prefix=$OPENSSL_PREFIX --libdir=lib -DPURIFY > build.log 2>&1
+make -j$(nproc)
+sudo make install_sw
+tar -czf openssl-$OPENSSL_VER-x64-focal.tar.gz /opt/ssl
+
