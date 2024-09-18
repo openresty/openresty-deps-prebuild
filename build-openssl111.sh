a9c30a7e69142d1a8923e5260ac6ee31d356758e
lijunlong <lijunlong@openresty.com>
2024-09-18 12:30:27 +0000

    bugfix: install to local command.

--- build-openssl111.sh
+++ build-openssl111.sh
@@ -9,6 +9,7 @@ cd openssl-$OPENSSL_VER
 patch -p1 < ../openresty/patches/openssl-$OPENSSL_PATCH_VER-sess_set_get_cb_yield.patch;
 ./config shared enable-ssl3 enable-ssl3-method -g --prefix=$OPENSSL_PREFIX --libdir=lib -DPURIFY > build.log 2>&1
 make -j$(nproc)
-sudo make install_sw
-tar -czf openssl-$OPENSSL_VER-x64-focal.tar.gz /opt/ssl
+mkdir fakeroot
+make install_sw DESTDIR=fakeroot
+tar -czf openssl-$OPENSSL_VER-x64-focal.tar.gz fakeroot opt
 
